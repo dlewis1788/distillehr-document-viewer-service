5a2a8a59ecc867bf37b8b1796ad24d60a7633eea
Derek Lewis <derek_lewis@projectivesoftware.com>
2017-03-04 09:30:14 -0600

    Initial import

---
+++ src/main/java/com/projectivesoftware/viewer/DistillehrDocumentViewerServiceApplication.java
@@ -0,0 +1,12 @@
+package com.projectivesoftware.viewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistillehrDocumentViewerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistillehrDocumentViewerServiceApplication.class, args);
+	}
+}
